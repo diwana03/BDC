7c3b754fd65933bf5e7f46e9bf240070269d7875
diwana03 <dhar.ashish03@gmail.com>
2026-09-10 15:06:20 +0800

    dev732 update tests/test-dance-cup-theme-live-contestant-v499.js

--- tests/test-dance-cup-theme-live-contestant-v499.js
+++ tests/test-dance-cup-theme-live-contestant-v499.js
@@ -3,10 +3,8 @@ const assert = require('assert');
 
 const control = fs.readFileSync('admin/dance-cup/projection-control.php', 'utf8');
 
-assert(
-  control.includes("$allowed=['holding','contestant','judges','contestants','scoring','results','podium']"),
-  'theme updates must accept the singular live contestant screen state'
-);
+const allowedScreens = control.match(/\$allowed=\[([^\]]+)\]/)?.[1] || '';
+assert(allowedScreens.includes("'contestant'"), 'theme updates must accept the singular live contestant screen state');
 assert(
   control.includes("SET theme=:theme,state_version=state_version+1") && !control.match(/action==='theme'[\s\S]{0,500}screen_type=:screen/),
   'theme updates must preserve the active projector screen'
